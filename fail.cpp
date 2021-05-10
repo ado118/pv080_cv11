9d38df15751760568bf04107bbe7c2cbe2b43615
ado118 <83953804+ado118@users.noreply.github.com>
2021-05-10 12:43:42 +0200

    Update fail.cpp

--- fail.cpp
+++ fail.cpp
@@ -4,8 +4,8 @@ int memcheckFailDemo(int* arrayStack, unsigned int arrayStackLen,
 	int* arrayHeap, unsigned int arrayHeapLen) {
   int Stack[5];
 
-  Static[100] = 0; 
-  Stack[100] = 0; 
+  Static[0] = 0; 
+  Stack[0] = 0; 
 
   for (int i = 0; i <= 5; i++) Stack [i] = 0;
 
